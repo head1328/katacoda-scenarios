ac9b3feff374549d019c3dd7fc419ab23d7b2699
Kevin Horst <info@kevinhorst.de>
2020-04-15 13:50:19 +0200

    example 1

--- kubernetes-fundamentals/nginx-fpm-pod/step1-verify.sh
+++ kubernetes-fundamentals/nginx-fpm-pod/step1-verify.sh
@@ -1,1 +1,1 @@
-(( $(docker images | grep "php\-app.*example\-1" 2>/dev/null | wc -l) >= 1 )) && echo \"done\"
+(( $(curl -fsSL $REGISTRY/v2/php-app/tags/list | grep "php\-app.*example\-1" 2>/dev/null | wc -l) >= 1 )) && echo \"done\"
